0d0aa7b977a52e482f4e7606046faf4ac610bef8
Andres Campuzano <campuxano88@gmail.com>
2020-05-27 12:17:30 -0500

    Tailwind added.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-blue-400">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -9,7 +9,7 @@
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
     HelloWorld
   }
@@ -22,7 +22,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
 }
 </style>
